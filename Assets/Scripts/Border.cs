ecbf67da60780edfec927d4ca573233dc31067cc
Kai Kuehner <kvk@pobox.com>
2018-04-21 21:15:39 -0400

    gl line stuff probably won't work

---
+++ Assets/Scripts/Border.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Border : MonoBehaviour {
+
+	public Material mat;
+	private Vector3 startVertex;
+	private Vector3 mousePos;
+
+	private void Start()
+	{
+		//mat = GetComponent<MeshRenderer>().material;
+	}
+
+	private void Update()
+	{
+		mousePos = Input.mousePosition;
+		if (Input.GetKeyDown(KeyCode.Space))
+			startVertex = new Vector3(mousePos.x / Screen.width, mousePos.y / Screen.height, 0);
+
+	}
+
+	private void OnPostRender()
+	{
+		GL.PushMatrix();
+		mat.SetPass(0);
+		GL.LoadOrtho();
+		GL.Begin(GL.LINES);
+		GL.Color(Color.red);
+		//GL.Vertex(startVertex);
+		//GL.Vertex(new Vector3(mousePos.x / Screen.width, mousePos.y / Screen.height, 0));
+		GL.Vertex(new Vector3(0, 0, 0));
+		GL.Vertex(new Vector3(5, 5, 5));
+		GL.End();
+		GL.PopMatrix();
+	}
+}
